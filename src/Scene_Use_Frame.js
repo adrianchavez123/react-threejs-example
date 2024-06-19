e8a1cfc76f3e041b3e7f74e32e31411de27be4d1
Manuel Adrian Chavez Sanchez <manuel.adrian.chavez@ibm.com>
2024-06-19 16:31:18 -0600

    add triangule with positions/vertices

--- src/Scene_Use_Frame.js
+++ src/Scene_Use_Frame.js
@@ -1,24 +1,27 @@
 import { useRef } from "react";
 import { useFrame, extend, useThree } from "@react-three/fiber";
 import * as THREE from "three";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { OrbitControls } from "@react-three/drei";
-import Custom from "./Custom";
 extend({ OrbitControls: OrbitControls });
 const Scene = () => {
   const cubeRef = useRef();
   const planeRef = useRef();
   const { gl, camera } = useThree();
+  // camera.position.x = 5;
+  useFrame((state, delta) => {
+    cubeRef.current.rotation.y += delta;
+    planeRef.current.rotation.x += delta;
 
-  useFrame((state, delta) => {});
+    state.camera.position.x = Math.sin(state.clock.elapsedTime);
+  });
   return (
     <>
-      <axesHelper args={[3]} />
-      <gridHelper args={[20, 20, 0xff0000, "cyan"]} />
+      {/* <orbitControls args={[camera, gl.domElement]} /> */}
       <OrbitControls />
-      <Custom />
       <mesh ref={planeRef} position-x={-2}>
-        <boxGeometry />
-        <meshBasicMaterial color="orange" side={THREE.DoubleSide} />
+        <planeGeometry args={[2, 5]} />
+        <meshBasicMaterial color="orange" wireframe side={THREE.DoubleSide} />
       </mesh>
       <mesh ref={cubeRef} position-x={2}>
         <boxGeometry />
--- src/index.js
+++ src/index.js
@@ -6,15 +6,26 @@ import { Canvas } from "@react-three/fiber";
 import "./styles.css";
 import Scene from "./Scene";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const creatingCanvasHandler = (state) => {
+  state.gl.setClearColor("cyan", 0.5);
+};
 root.render(
   <>
     <Canvas
+      gl={{
+        antialias: true,
+        alpha: true,
+      }}
+      orthographic
       camera={{
         fov: 45,
         near: 0.1,
         far: 100,
+        zoom: 120,
         // position: [2, 2, 5]
       }}
+      onCreated={creatingCanvasHandler}
     >
       <Scene />
     </Canvas>
