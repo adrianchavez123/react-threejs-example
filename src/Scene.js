cd2b221f7b8fd5a4e09a48ba6ce0c5b713ae94ee
Manuel Adrian Chavez Sanchez <manuel.adrian.chavez@ibm.com>
2024-06-19 16:04:01 -0600

    add orbitControls 1

--- src/Scene.js
+++ src/Scene.js
@@ -1,9 +1,30 @@
+import { useRef } from "react";
+import { useFrame, extend, useThree } from "@react-three/fiber";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+extend({ OrbitControls: OrbitControls });
 const Scene = () => {
+  const cubeRef = useRef();
+  const planeRef = useRef();
+  const { gl, camera } = useThree();
+  useFrame((state, delta) => {
+    // console.log("animate");
+    cubeRef.current.rotation.y += delta;
+    planeRef.current.rotation.x += delta;
+  });
   return (
-    <mesh>
-      <planeGeometry args={[2, 5]} />
-      <meshBasicMaterial />
-    </mesh>
+    <>
+      <orbitControls args={[camera, gl.domElement]} />
+      <mesh ref={planeRef} position-x={-2}>
+        <planeGeometry args={[2, 5]} />
+        <meshBasicMaterial color="orange" wireframe side={THREE.DoubleSide} />
+      </mesh>
+      <mesh ref={cubeRef} position-x={2}>
+        <boxGeometry />
+        <meshBasicMaterial color="purple" />
+      </mesh>
+    </>
   );
 };
 export default Scene;
