62c735cf60d26101bea1e52fe5379b9560860c5d
Manuel Adrian Chavez Sanchez <manuel.adrian.chavez@ibm.com>
2024-06-24 10:58:06 -0600

    load an image

--- src/Scene.js
+++ src/Scene.js
@@ -1,28 +1,14 @@
-import { useRef } from "react";
-import { useFrame, extend, useThree } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
 import * as THREE from "three";
-import { OrbitControls } from "@react-three/drei";
-import Custom from "./Custom";
-extend({ OrbitControls: OrbitControls });
-const Scene = () => {
-  const cubeRef = useRef();
-  const planeRef = useRef();
-  const { gl, camera } = useThree();
 
-  useFrame((state, delta) => {});
+const Scene = () => {
+  const texture = useLoader(THREE.TextureLoader, "./img/1.png");
+  console.log(texture);
   return (
     <>
-      <axesHelper args={[3]} />
-      <gridHelper args={[20, 20, 0xff0000, "cyan"]} />
-      <OrbitControls />
-      <Custom />
-      <mesh ref={planeRef} position-x={-2}>
-        <boxGeometry />
-        <meshBasicMaterial color="orange" side={THREE.DoubleSide} />
-      </mesh>
-      <mesh ref={cubeRef} position-x={2}>
-        <boxGeometry />
-        <meshBasicMaterial color="purple" />
+      <mesh>
+        <planeGeometry args={[4, 4]} />
+        <meshBasicMaterial map={texture} />
       </mesh>
     </>
   );
