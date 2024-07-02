7cafe289d9bd2c8f876e45ecd081a26f6f777b4b
Manuel Adrian Chavez Sanchez <manuel.adrian.chavez@ibm.com>
2024-07-01 20:33:39 -0600

    adding text on threejs

---
+++ src/Scene_PivotControl.js
@@ -0,0 +1,20 @@
+import CameraControl from "./CameraControl";
+import OrbitControl from "./OrbitControl";
+import PivotControl from "./PivotControl";
+import PresentationControl from "./PresentationControl";
+import ScrollControl from "./ScrollControl";
+import TransformControl from "./TransformControl";
+const Scene = () => {
+  return (
+    <>
+      {/* <CameraControl /> */}
+      {/* <OrbitControl /> */}
+      {/* <PresentationControl /> */}
+      {/* <ScrollControl /> */}
+      {/* <TransformControl /> */}
+      <PivotControl />
+    </>
+  );
+};
+
+export default Scene;
